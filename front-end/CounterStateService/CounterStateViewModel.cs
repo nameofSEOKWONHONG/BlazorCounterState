b0e175fd08b27e892c2865a35656c5f7d7e0e483
SeokWon Hong <h20913@gmail.com>
2021-10-06 22:17:04 +0900

    first commit

---
+++ front-end/CounterStateService/CounterStateViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Components;
+
+namespace CounterState
+{
+    public class CounterStateViewModel : ICounterStateViewModel
+    {
+        private int _count;
+        public int Count
+        {
+            get => _count;
+            set
+            {
+                _count = value;
+                OnPropertyChanged();
+            } 
+        }
+        public void Increment()
+        {
+            Count += 1;
+        }
+
+        public void Decrement()
+        {
+            Count -= 1;
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }    
+}
+
---
+++ front-end/CounterStateService/ICounterStateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.AspNetCore.Components;
+
+namespace CounterState
+{
+    public interface ICounterStateViewModel : INotifyPropertyChanged
+    {
+        int Count { get; set; }
+        void Increment();
+        void Decrement();
+    }
+}
